02bc6afdcb4a6e729ac7ae45c221ef60f20fb209
Your Name <you@example.com>
2020-02-08 16:06:32 +0100

    Po szkoleniu

--- src/post.js
+++ src/post.js
@@ -1,79 +1,78 @@
-let posts=window.posts
+let posts = window.posts
 
 
-let post1={
-    name: posts[0],
+let post1 = {
+    post: posts[0],
     comments: [
-        { 
-        id: 1,
-        body: 'Laboriosam aliquid consequuntur, voluptate quibusdam temporibus doloremque quam illo i ipsum.....',
-        author: 'Artur Bednarek'
+        {
+            id: 1,
+            body: 'Laboriosam aliquid consequuntur, voluptate quibusdam temporibus doloremque quam illo i ipsum.....',
+            author: 'Artur Bednarek'
         },
         {
-        id: 2,
-        body: 'Numquam ipsam ad aperiam nemo dicta in enim reprehenderit quisquam quas dolores, eum repudiandae sed saepe!  ipsum.....',
-        author: 'Anna Nowak'
+            id: 2,
+            body: 'Numquam ipsam ad aperiam nemo dicta in enim reprehenderit quisquam quas dolores, eum repudiandae sed saepe!  ipsum.....',
+            author: 'Anna Nowak'
         },
         {
-        id: 3,
-        body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, accusamus culpa vel nulla numquam maxime voluptatem odit pariatur recusandae.',
-        author: 'Aleksandra Kowalska'
+            id: 3,
+            body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore cupiditate, accusamus culpa vel nulla numquam maxime voluptatem odit pariatur recusandae.',
+            author: 'Aleksandra Kowalska'
         }
     ]
 }
 
 
-createPost()
+createPost(post1.post)
+createComments(postItem, post1)
 
 
-function createPost(){
-    
-    postItem.innerHTML=''
-    let post= post1.name;
-    let postTitle=document.createElement('h4')
-    let postBody=document.createElement('p')
-    let postFooter=document.createElement('footer')
-    
+function createPost(post) {
+    postItem.innerHTML = ''
+
+    let postTitle = document.createElement('h4')
+    let postBody = document.createElement('p')
+    let postFooter = document.createElement('footer')
+    let postComments = document.createElement('ul')
+        postComments.classList.add('comments-list')
+
     postBody.classList.add('mb-0');
     postFooter.classList.add('blockquote-footer');
 
-    let titleText=post.title
-    let bodyText=post.body
-    let footerText=post.author.username
+    let titleText = post.title
+    let bodyText = post.body
+    let footerText = post.author.username
 
-    postTitle.innerText=titleText;
-    postBody.innerText=bodyText;
-    postFooter.innerText=footerText;
+    postTitle.innerText = titleText;
+    postBody.innerText = bodyText;
+    postFooter.innerText = footerText;
 
     postItem.appendChild(postTitle);
     postItem.appendChild(postBody);
     postItem.appendChild(postFooter);
+    postItem.appendChild(postComments);
 
     return postItem
 }
 
-createComment()
 
-function createComment(){
-    let comments = post1.comments
+function createComments(postItem, post) {
+    let comments = post.comments
+    let ul = postItem.querySelector("ul.comments-list");
+    ul.innerHTML = ''
+    
+    let tpl = document.getElementById('commentTpl').content
 
-    for (let i in comments){
-        let ul = document.createElement("UL");
-        let li = document.createElement("LI");
-        let span = document.createElement("span");
-        span.classList.add('blockquote-footer')
-        let text=comments[i].body;
-        let author=comments[i].author;
-        li.innerText=text;
-        span.innerText=author;
-        li.appendChild(span);
+    for (let i in comments) {
+        let comment = comments[i]
+        let li = tpl.cloneNode(true)
+
+        li.querySelector(".blockquote-footer").innerText = comment.author;
+        li.querySelector(".comment-text").innerText = comment.body;
+
         ul.appendChild(li);
-    
-
-        postItem.appendChild(ul)
-    
-        }
-        return postItem
+    }
+    return postItem
 }
 
 
